6f731f9efd995e5491a511b57369fc45c1df1960
Tetsuaki Baba <tetsuaki.baba@gmail.com>
2020-08-25 10:35:50 +0900

    update README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple Example for selecting video/audio devices works only chrome browser.
+
+## How to use
+```
+$ git clone https://github.com/TetsuakiBaba/selectVideoAudioDevices.git
+$ cd seleceVideoAudioDevices
+$ npm init
+$ npm install express
+$ node server.js
+```
+Then check http://localhost:80 on Chrome browser.
+![](./sample.png)
--- public/sketch.js
+++ public/sketch.js
@@ -3,7 +3,15 @@ function setup() {
     // set all available video/audio devices to #videoSource
     attachVideoDevicesToSelect("#videoSource");
     attachAudioDevicesToSelect("#audioSource");
+    var video_device = document.getElementById("videoSource");
+    console.log(video_device);
+    select("#videoSource").changed(changedVideoDevice);
 }
 
 function draw() {
+
+}
+
+function changedVideoDevice() {
+    console.log('Device Id:', this.value());
 }
